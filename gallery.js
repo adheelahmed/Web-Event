62139912c624e4cde7e1973f7bb81cd7c2a4e5ed
Adheel Ahmed <75260527+AdheelAhmed-D3CD@users.noreply.github.com>
2021-05-06 05:27:45 +0530

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -21,3 +21,21 @@
    	 });
    }
  });
+
+
+ jQuery($ => {
+    const speed = 400;
+
+    $('a[href*="#"]')
+      .filter((i, a) => a.getAttribute('href').startsWith('#') || a.href.startsWith(`${location.href}#`))
+      .unbind('click.smoothScroll')
+      .bind('click.smoothScroll', event => {
+        const targetId = event.currentTarget.getAttribute('href').split('#')[1];
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+          event.preventDefault();
+          $('html, body').animate({ scrollTop: $(targetElement).offset().top }, speed);
+        }
+      });
+  });
